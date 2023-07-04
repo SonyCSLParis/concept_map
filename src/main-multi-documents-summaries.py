31d01694a2cc73d4b56f73250c720a0fee73e123
martina <martinagalletti@icloud.com>
2023-07-04 11:54:42 +0200

    importance-ranking + two mains

--- src/main-multi-documents-summaries.py
+++ src/main-multi-documents-summaries.py
@@ -7,15 +7,15 @@ from evaluation_test_set import *
 from coref import *
 from importance_ranking import *
 
+### ---------------------------------------------------------- MULTIDOCUMENT SUMMARISATION ----------------------------------------------------------
+
 if __name__ == '__main__':
 
-   ### MULTIDOCUMENT SUMMARISATION ----------------------------------------------------------
+    ### 1.1. TRAIN
 
-    ### TRAIN
-
-    # generate summaries
-    # parent_folder_path = CMAP_TRAIN_DIR
-    # summarize_subfolders(parent_folder_path)
+    #generate summaries
+    parent_folder_path = CMAP_TRAIN_DIR
+    summarize_subfolders(parent_folder_path)
 
    #  parent_folder_path = SUMMARIES
    #  output_folder_path = IMPORTANCE_RANKING
@@ -36,10 +36,7 @@ if __name__ == '__main__':
     # output_folder_path = OUTPUT_DIR_GRAPH
     # generate_graph_from_txt_files(folder_path, output_folder_path)
 
-
-    ### EVALUATION ----------------------------------------------------------
-
-    ## RUN ON TEST
+    ### 1.2. TEST
 
     # generate summaries
     # parent_folder_path = CMAP_TEST_DIR
@@ -47,15 +44,15 @@ if __name__ == '__main__':
     # summarize_subfolders(parent_folder_path,output_folder_path)
     # summarize_subfolders(CMAP_TEST_DIR,SUMMARIES)
 
-   # sentence importances
-   #  parent_folder_path = SUMMARIES
-   #  output_folder_path = IMPORTANCE_RANKING
-   #  process_parent_folder(parent_folder_path,output_folder_path)
+    # sentence importances
+    #  parent_folder_path = SUMMARIES
+    #  output_folder_path = IMPORTANCE_RANKING
+    #  process_parent_folder(parent_folder_path,output_folder_path)
 
     # extract triplets from summaries
-    parent_folder_path = IMPORTANCE_RANKING
-    output_folder_path = OUTPUT_DIR_TRIPLETS_DISJOINT_TEST
-    extract_triplets_from_summaries(parent_folder_path, output_folder_path)
+    # parent_folder_path = IMPORTANCE_RANKING
+    # output_folder_path = OUTPUT_DIR_TRIPLETS_DISJOINT_TEST
+    # extract_triplets_from_summaries(parent_folder_path, output_folder_path)
 
     # aggregate triplets
     # parent_folder_path_disjoint = OUTPUT_DIR_TRIPLETS_DISJOINT_TEST
@@ -66,14 +63,12 @@ if __name__ == '__main__':
     # folder_path = OUTPUT_DIR_TRIPLETS_AGGREGATE_TEST
     # output_folder_path = OUTPUT_DIR_GRAPH_TEST
     # generate_graph_from_txt_files(folder_path, output_folder_path)
-    #
 
-    # METEOR AND ROUGE
+    ### 1.3. METEOR AND ROUGE
 
     # input_folder_path = OUTPUT_DIR_TRIPLETS_AGGREGATE_TEST
     # gold_folder_path = CMAP_FINAL_TEST_DIR
     # output_folder_path = OUTPUT_DIR_EVALUATION_CSV
-    #
     # compute_metrics(input_folder_path, gold_folder_path, output_folder_path)
 
     # METEOR AND ROUGE CHATGPT
@@ -81,23 +76,3 @@ if __name__ == '__main__':
     # output_folder_path = CMAP_TEST_GPT
     # aggregate_txt_files(input_folder_path, output_folder_path)
 
-### SINGLEDOCUMENT SUMMARISATION ----------------------------------------------------------
-
-    # generate summaries
-    # parent_folder_path = WIKIPEDIA_TRAIN_DIR
-    # summarize_folder(parent_folder_path)
-
-   # coref
-   # parent_folder_path = OUTPUT_DIR_SINGLE_DOC_DISJOINT
-   # output_folder_path = OUTPUT_DIR_COREF
-   # perform_coreference_resolution(parent_folder_path, output_folder_path)
-
-   # extract triplets from summaries
-   # parent_folder_path = WIKIPEDIA_TRAIN_DIR
-   # output_folder_path = OUTPUT_DIR_SINGLE_DOC_DISJOINT
-   # extract_triplets_from_single_txt(parent_folder_path, output_folder_path)
-
-   # generate graph
-   #  folder_path = OUTPUT_DIR_SINGLE_DOC_DISJOINT
-   #  output_folder_path = OUTPUT_DIR_GRAPH_SINGLE_DOC
-   #  generate_graph_from_txt_files(folder_path, output_folder_path)
---
+++ src/main-single-documents-summaries.py
@@ -0,0 +1,34 @@
+from summary_generation import *
+from coref import *
+from importance_ranking import *
+
+### ----------------------------------------------------------  SINGLE DOCUMNET SUMMARISATION ----------------------------------------------------------
+
+if __name__ == '__main__':
+
+    ### 2.1. TRAIN
+
+    # generate summaries
+    parent_folder_path = WIKIPEDIA_TRAIN_DIR
+    output_folder_path = OUTPUT_DIR_SUMMARIES
+    summarize_folder(parent_folder_path, output_folder_path)
+
+    # coref
+    parent_folder_path = OUTPUT_DIR_SUMMARIES
+    output_folder_path = OUTPUT_DIR_COREF
+    perform_coreference_resolution(parent_folder_path, output_folder_path)
+
+    # sentence importances
+    parent_folder_path = OUTPUT_DIR_COREF
+    output_folder_path = OUTPUT_IMPORTANCE_RANKING
+    process_parent_folder(parent_folder_path, output_folder_path)
+
+    # extract triplets from summaries
+    #  parent_folder_path = OUTPUT_IMPORTANCE_RANKING
+    #  output_folder_path = OUTPUT_DIR_SINGLE_DOC_DISJOINT
+    #  extract_triplets_from_summaries(parent_folder_path, output_folder_path)
+
+    # generate graph
+    #  folder_path = OUTPUT_DIR_SINGLE_DOC_DISJOINT
+    #  output_folder_path = OUTPUT_DIR_GRAPH_SINGLE_DOC
+    #  generate_graph_from_txt_files(folder_path, output_folder_path)
